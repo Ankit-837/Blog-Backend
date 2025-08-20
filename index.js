6bf61aa36039b47af605698a5cd786c6faa9c598
Ankit-837 <choprankit0837@gmail.com>
2025-08-20 09:54:02 +0530

    some updated and index file attached

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://blog-front-five-tau.vercel.app/"],
+    origin: ["https://blog-front-five-tau.vercel.app"],
     credentials: true,
   })
 );
@@ -20,6 +25,9 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates", "index.html"));
+});
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
